687287e70b32a9f6bfd07cfffa5ff004f30e2230
JIHYE SEO <138651699+swisdom784@users.noreply.github.com>
2026-06-12 18:30:25 +0900

    Make DataScreen clickable without action

--- app/src/main/java/com/samsung/smartclipboard/presentation/main/data/DataScreen.kt
+++ app/src/main/java/com/samsung/smartclipboard/presentation/main/data/DataScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -562,6 +563,11 @@ private fun PreviewOverlay(
         Modifier
             .fillMaxSize()
             .background(AppColors.Slate900)
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = {}
+            )
             .padding(16.dp),
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
