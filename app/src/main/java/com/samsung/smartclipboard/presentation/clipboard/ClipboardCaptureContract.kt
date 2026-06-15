88ccbe7d1f0e767760e27746f87e13ce540790b8
ysh020702 <168499494+ysh020702@users.noreply.github.com>
2026-06-15 14:00:12 +0900

    Delete app/src/main/java/com/samsung/smartclipboard/presentation/clipboard directory

--- app/src/main/java/com/samsung/smartclipboard/presentation/clipboard/ClipboardCaptureContract.kt
+++
@@ -1,9 +0,0 @@
-package com.samsung.smartclipboard.presentation.clipboard
-
-data class ClipboardCaptureUiState(
-    val isProcessing: Boolean = true,
-    val message: String = "Saving clipboard...",
-    val savedCount: Int = 0,
-    val shouldFinish: Boolean = false,
-    val isSuccess: Boolean = false
-)
